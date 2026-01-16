ca1f931c7917cdef684bb3a19c5f0d0ecdbb1332
Kelvin Cheng <kelvincheng2009@gmail.com>
2026-01-16 16:14:05 +1100

    finished 3.7-3.8

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const app = express()
 
+const morgan = require('morgan')
+morgan.token('body', (req) => JSON.stringify(req.body))
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+
 app.use(express.json())
 let persons = [
     { 
