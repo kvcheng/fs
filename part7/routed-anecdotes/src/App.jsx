bc3da4ff1294a38c3ff0826d1f4567250bde705c
Kelvin Cheng <kelvincheng2009@gmail.com>
2026-02-10 21:29:13 +1100

    finished up to 7.6

--- part7/routed-anecdotes/src/App.jsx
+++ part7/routed-anecdotes/src/App.jsx
@@ -6,6 +6,7 @@ import {
   useMatch,
   useNavigate
 } from 'react-router-dom'
+import { useField } from './hooks'
 
 const Menu = () => {
   const padding = {
@@ -71,24 +72,25 @@ const Footer = () => (
 )
 
 const CreateNew = (props) => {
-  const [content, setContent] = useState('')
-  const [author, setAuthor] = useState('')
-  const [info, setInfo] = useState('')
+  const content = useField('text')
+  const author = useField('text')
+  const info = useField('text')
+  // const [content, setContent] = useState('')
+  // const [author, setAuthor] = useState('')
+  // const [info, setInfo] = useState('')
   const navigate = useNavigate()
 
 
   const handleSubmit = (e) => {
     e.preventDefault()
     props.addNew({
-      content,
-      author,
-      info,
+      content: content.value,
+      author: author.value,
+      info: info.value,
       votes: 0
     })
-    if (props.setNotification) {
-      props.setNotification(`A new anecdote '${content}' created!`)
-      setTimeout(() => props.setNotification(''), 5000)
-    }
+    props.setNotification(`A new anecdote '${content.value}' created!`)
+    setTimeout(() => props.setNotification(''), 5000)
     navigate('/')
   }
 
@@ -98,17 +100,22 @@ const CreateNew = (props) => {
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input name='content' value={content} onChange={(e) => setContent(e.target.value)} />
+          <input {...content} />
         </div>
         <div>
           author
-          <input name='author' value={author} onChange={(e) => setAuthor(e.target.value)} />
+          <input {...author} />
         </div>
         <div>
           url for more info
-          <input name='info' value={info} onChange={(e)=> setInfo(e.target.value)} />
+          <input {...info} />
         </div>
         <button>create</button>
+        <button type='button' onClick={() => {
+          content.onReset()
+          author.onReset()
+          info.onReset()
+        }}>reset</button>
       </form>
     </div>
   )
